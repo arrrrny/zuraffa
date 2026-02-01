f662c498cef677b59d8217d147fa3d23e6680ab3
ARRRRNY <ahmettok@gmail.com>
2026-02-01 10:36:46 +0300

    chore: release 1.11.0

--- lib/src/zfa_cli.dart
+++ lib/src/zfa_cli.dart
@@ -5,7 +5,7 @@ import 'commands/validate_command.dart';
 import 'commands/create_command.dart';
 import 'commands/initialize_command.dart';
 
-const version = '1.10.0';
+const version = '1.11.0';
 
 Future<void> run(List<String> args) async {
   if (args.isEmpty) {
