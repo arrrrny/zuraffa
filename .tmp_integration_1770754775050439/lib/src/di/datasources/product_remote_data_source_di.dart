80c20992c498793bf970f5d0448f5a94b45e2f58
ARRRRNY <ahmettok@gmail.com>
2026-02-11 00:59:58 +0300

    removed test files

--- .tmp_integration_1770754775050439/lib/src/di/datasources/product_remote_data_source_di.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-
-import '../../data/data_sources/product/product_remote_data_source.dart';
-
-void registerProductRemoteDataSource(GetIt getIt) {
-  getIt.registerLazySingleton<ProductRemoteDataSource>(
-    () => ProductRemoteDataSource(),
-  );
-}
